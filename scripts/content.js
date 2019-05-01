054a742760619f9bfd17fefee2d99c96fa272598
Hieu C. Nguyen <nc.hieu@live.com>
2019-05-01 12:18:18 +1000

    [Feature] Added observeDOMChanges (lazy load elements)

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,77 @@
+var observer;
+var keywords = [];
+
+function isContentMatchKeywords(content) {
+
+    var rs = false;
+    content = content.replace(/\s/g, "").toLowerCase();
+
+    keywords.some(term => {
+        if (content.indexOf(term.replace(/\s/g, "").toLowerCase()) !== -1) {
+            rs = true;
+        }
+        return rs;
+    });
+    return rs;
+}
+
+function recursiveFilter(node, opts) {
+
+    if (node.childElementCount) {
+        for (var i = 0; i < node.childElementCount; i++) {
+            recursiveFilter(node.children[i], opts);
+        }
+    } else {
+        if (node && isContentMatchKeywords(node.textContent)) {
+            opts.results.push(node);
+        }
+    }
+}
+
+function observeDOMChanges() {
+
+    observer = new MutationObserver(function (mutationsList, observer) {
+        let opts = { results: [] };
+
+        for (var mutation of mutationsList) {
+            mutation.addedNodes.forEach(node => {
+                recursiveFilter(node, opts);
+            })
+        };
+
+        opts.results.forEach(node => {
+            
+            if (node.style) {
+                node.style.opacity = 0;
+            } else {
+                console.log("No style", node);
+            }
+        })
+    });
+
+    // Start observing the target node for configured mutations
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}
+
 function refresh() {
 
     chrome.storage.sync.get(['items'], function (result) {
+
+        keywords = result.items;
+
         var all = document.querySelectorAll("h1,p,h3,h2,a,span");
-        var items = result.items;
+
         var i = 0;
         all.forEach(el => {
-            let content = el.innerText.replace(/\s/g, "").toLowerCase();
-            items.some(term => {
-                if (content.indexOf(term.replace(/\s/g, "")) !== -1) {
-                    el.style.background = "transparent";
-                    el.style.color = "transparent";
-                    i++;
-                    return true;
-                }
-                return false;
-            })
+
+            if (isContentMatchKeywords(el.innerText)) {
+                el.style.background = "transparent";
+                el.style.color = "transparent";
+                i++;
+            }
         });
 
         // console.log("No Spoiler removes", i, "related content");
@@ -22,8 +79,10 @@ function refresh() {
     })
 }
 
-
-setTimeout(refresh, 3000);
+setTimeout(() => {
+    refresh();
+    observeDOMChanges();
+}, 3000);
 
 var frameId = "no-spoider-frame";
 var iframe = document.createElement('iframe');
@@ -40,7 +99,7 @@ document.body.appendChild(iframe);
 
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	if (request.type == "nsrefresh") {
-		refresh();
-	}
+    if (request.type == "nsrefresh") {
+        refresh();
+    }
 });
